d006758bfa0e4935b084927faa6feebb723890cd
Sandip040401 <iemsandip@gmail.com>
2025-07-18 12:20:23 +0530

    APIS DONE

--- routes/assignmentRoutes.js
+++ routes/assignmentRoutes.js
@@ -1,8 +1,11 @@
 // routes/assignments.js
 import express from "express";
-import { generateCodingAssignment } from "../controller/assignmentController.js";
+import { generateCodingAssignment, getAllCodingAssignments, getSingleCodingAssignment, saveCodingAssignment } from "../controller/assignmentController.js";
 const router = express.Router();
 
 router.post("/generate", generateCodingAssignment);
+router.post("/save", saveCodingAssignment); // Save to DB
+router.get("/", getAllCodingAssignments); // All assignments
+router.get("/:id", getSingleCodingAssignment); // One assignment by ID
 
 export default router;
